c2a502f7f90fcb364c1dc2017e62c20058b8e623
Eduardo de Paula <eduardoibarr56@gmail.com>
2024-01-21 12:28:40 -0300

    refactor (backend): add editorconfig and update eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,37 +1,38 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended'
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
     ],
-    'overrides': [
+    'linebreak-style': [
+      'error',
+      'windows'
     ],
-    'parserOptions': {
-        'ecmaVersion': 'latest'
-    },
-    'plugins': [
-        'react'
+    'quotes': [
+      'error',
+      'single'
     ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-undef': 'off',
+  }
 };
